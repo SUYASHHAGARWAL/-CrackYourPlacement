364e75c9c3f842087f357d11cc33ce10c1859cf6
suyashhagarwal <suyash1606.agarwal@gmail.com>
2024-07-17 23:38:49 +0530

    day1

---
+++ Array/Find_the_Duplicate_Number.cpp
@@ -0,0 +1,20 @@
+/*
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,bool> map;
+        for(int i=0;i<nums.size();i++){
+            if(map[nums[i]] == true){
+                return nums[i];
+            }
+            map[nums[i]] = true;
+        }
+        return -1;
+    }
+};
+
+
+
+*/
