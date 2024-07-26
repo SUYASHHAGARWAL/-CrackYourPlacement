19c19cb8da4c146cc6027abca65c099bd19858e7
suyashhagarwal <suyash1606.agarwal@gmail.com>
2024-07-26 22:10:18 +0530

    day10

---
+++ Stack and Queue/BackSpace_string_compare.cpp
@@ -0,0 +1,31 @@
+/*
+
+day10
+
+class Solution {
+public:
+     string processString( string& str){
+        stack<char>editor;
+        for(auto&c : str){
+            if(c != '#'){
+                editor.push(c);
+            }else if (!editor.empty()){// c=='#' so we are removing the letter bcoz backspace
+                editor.pop();
+            }
+        }
+        string result;
+        while(!editor.empty()){
+            result += editor.top();
+            editor.pop();
+        }
+       
+        return result;
+    }
+public:
+    bool backspaceCompare(string s, string t) {
+        return processString(s) == processString(t);
+    }
+};
+
+
+*/
