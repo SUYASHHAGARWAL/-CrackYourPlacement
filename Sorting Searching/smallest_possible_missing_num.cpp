b515953532d0b5bfe07decdfe4d1f70034d1abe7
suyashhagarwal <suyash1606.agarwal@gmail.com>
2024-08-03 14:29:27 +0530

    day18

---
+++ Sorting Searching/smallest_possible_missing_num.cpp
@@ -0,0 +1,26 @@
+/*
+
+day18
+
+class Solution
+{
+    public:
+    //Function to find the smallest positive number missing from the array.
+    int missingNumber(int arr[], int n) 
+    { 
+        // Your code here
+        set<int>s;
+        for(int i=0;i<n;i++){
+            s.insert(arr[i]);
+        }
+        
+        for(int i=1;i<=n;i++){
+            if(s.find(i)==s.end()){
+                return i;
+            }
+        }
+        return n+1;
+    } 
+};
+
+*/
