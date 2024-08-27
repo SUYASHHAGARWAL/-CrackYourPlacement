5f709678d787d034eb58a3ddbcb54689d25f515f
suyashhagarwal <suyash1606.agarwal@gmail.com>
2024-08-27 21:45:33 +0530

    day34

---
+++ heap/find_the_most_competetive_subsequence.cpp
@@ -0,0 +1,34 @@
+/*
+
+day34
+
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+       stack<int> q;
+
+       for(int i=0;i<nums.size();i++)
+       {
+           while(q.size()!=0&&q.size()+nums.size()-i-1>=k&&nums[i]<q.top())
+           q.pop();
+
+           if(q.size()<k) 
+           q.push(nums[i]);
+       }
+
+       vector<int> ans;
+
+       while(q.size()!=0)
+       {
+           ans.push_back(q.top());
+           q.pop();
+       }
+
+       reverse(ans.begin(),ans.end());
+
+       return ans;
+    }
+};
+
+
+*/
